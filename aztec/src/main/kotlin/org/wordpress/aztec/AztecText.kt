121a1b6bb89019bdee0378cd376a70c808462f57
khaykov <forsver@gmail.com>
2024-11-27 15:10:18 -0600

    Prevent selection of EOB marker.

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
@@ -1240,6 +1240,7 @@ open class AztecText : AppCompatEditText, TextWatcher, UnknownHtmlSpan.OnUnknown
         if (length() != 0) {
             // do not set selection when we try to select end of buffer marker in empty editor
             if (selStart == 0 && selEnd == 1 && (length() == 1 && text[0] == Constants.END_OF_BUFFER_MARKER)) {
+                deleteInlineStyleFromTheBeginning()
                 return
             }
 
